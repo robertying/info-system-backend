453633e29f20fdd38dce671b0b8111a88dcf9bef
Robert Ying <yingrui205@gmail.com>
2018-09-16 01:26:45 -0600

    Update honor route

--- routes/honors.js
+++ routes/honors.js
@@ -1,57 +1,120 @@
 const express = require("express");
-
+const _ = require("lodash");
 const Honor = require("../models/honor");
-const modelExistsVerifier = require("../helpers/existsVerifier");
+const existenceVerifier = require("../helpers/existenceVerifier");
 const verifyToken = require("../middlewares/verifyToken");
-const authorizationVerifier = require("../middlewares/authorizationVerifier");
+const verifyAuthorizations = require("../middlewares/verifyAuthorizations");
 
 const router = express.Router();
 
 /**
- * Get a list of honors.
- * @param {string} token
- * @param {number} begin
- * @param {number} end
- * @returns {Honor[]}
+ * GET
+ * 获得所有荣誉
  */
-router.post("/", verifyToken, (req, res) => {
-  const query = Honor.find({})
-    .skip(req.body.begin - 1)
-    .limit(req.body.end - req.body.begin + 1);
-  query.exec((err, honors) => {
+router.get("/", verifyToken, (req, res) => {
+  Honor.find({}, (err, honors) => {
     if (err) {
-      res.status(500).send("Internal server error");
+      res.status(500).send("500 Internal server error.");
     } else {
+      const result = honors.map(n => {
+        let honor = {};
+        honor.id = n._id;
+        honor.title = n.title;
+        return honor;
+      });
       res.setHeader("Content-Type", "application/json; charset=utf-8");
-      res.end(JSON.stringify(honors));
+      res.status(200).end(JSON.stringify(result));
     }
   });
 });
 
 /**
- * Add a new type of honor.
- * @param {string} token
- * @param {Honor} req.body
- * @returns {Honor}
+ * GET
+ * 获得特定荣誉
+ * @param {String} id 荣誉 ID
+ * @returns {JSON} 特定荣誉
  */
-router.post(
-  "/new",
-  verifyToken,
-  authorizationVerifier("write"),
-  async (req, res) => {
-    const honorExists = await modelExistsVerifier(Honor, req.body.id);
-    if (honorExists === null) {
-      res.status(500).send("Internal error");
-    } else if (honorExists === true) {
-      res.status(409).send("Model already exists");
+router.get("/:id", verifyToken, (req, res) => {
+  Honor.findById(req.params.id, (err, honor) => {
+    if (err) {
+      res.status(500).send("500 Internal server error.");
+    } else if (!honor) {
+      res.status(404).send("404 Not Found: Honor does not exist.");
     } else {
+      let returnedHonor = {};
+      returnedHonor.id = honor._id;
+      returnedHonor.title = honor.title;
+
+      res.setHeader("Content-Type", "application/json; charset=utf-8");
+      res.status(200).end(JSON.stringify(returnedHonor));
+    }
+  });
+});
+
+/**
+ * POST
+ * 新增荣誉
+ * @returns {String} Location header
+ */
+// TODO: Add situation when same ID submit
+router.post(
+  "/",
+  verifyToken,
+  verifyAuthorizations(["write"]),
+  async (req, res) => {
+    req.body.createdBy = req.id;
+    const newHonor = new Honor(req.body);
+    newHonor.save((err, honor) => {
+      if (err) {
+        res.status(500).send("500 Internal server error.");
+      } else {
+        res.setHeader("Location", "/honors/" + honor._id);
+        res.status(201).send("201 Created.");
+      }
+    });
+  }
+);
+
+/**
+ * PUT
+ * 更新荣誉
+ * @param {String} id 需要更新的荣誉 ID
+ * @returns {String} Location header 或空
+ */
+router.put(
+  "/:id",
+  verifyToken,
+  verifyAuthorizations(["write"]),
+  async (req, res) => {
+    const honorExists = await existenceVerifier(Honor, {
+      _id: req.params.id
+    });
+
+    if (honorExists === null) {
+      res.status(500).send("500 Internal server error.");
+    } else if (honorExists === false) {
       const newHonor = new Honor(req.body);
+
       newHonor.save((err, honor) => {
         if (err) {
-          res.status(500).send("Internal error");
+          res.status(500).send("500 Internal server error.");
         } else {
-          res.setHeader("Content-Type", "application/json; charset=utf-8");
-          res.end(JSON.stringify(honor));
+          res.setHeader("Location", "/honors/" + honor._id);
+          res.status(201).send("201 Created.");
+        }
+      });
+    } else {
+      const honor = honorExists;
+      _.merge(honor, req.body);
+      Object.entries(req.body).forEach(([key]) => honor.markModified(key));
+      honor.updatedAt = new Date().toISOString();
+      honor.updatedBy = req.id;
+
+      honor.save(err => {
+        if (err) {
+          res.status(500).send("500 Internal server error.");
+        } else {
+          res.status(204).send("204 No Content.");
         }
       });
     }
@@ -59,87 +122,25 @@ router.post(
 );
 
 /**
- * Get a specific financial aid of {:id}.
- * @param {string} token
- * @param {number} id
- * @returns {Honor}
- */
-router.get("/:id", verifyToken, (req, res) => {
-  Honor.findOne({ id: req.params.id }, (err, honor) => {
-    if (err) {
-      res.status(500).send("Internal error");
-    } else {
-      res.setHeader("Content-Type", "application/json; charset=utf-8");
-      res.end(JSON.stringify(honor));
-    }
-  });
-});
-
-/**
- * Update an existing honor.
- * @param {string} token
- * @param {number} id
- * @param {Honor} req.body
- * @returns {Honor}
- */
-router.put("/:id", verifyToken, authorizationVerifier("write"), (req, res) => {
-  const honorExists = modelExistsVerifier(Honor, req.body.id);
-  if (honorExists === null) {
-    res.status(500).send("Internal error");
-  } else if (honorExists === false) {
-    res.status(404).send("Model not found");
-  } else {
-    Honor.findOne({ id: req.params.id }, (err, honor) => {
-      if (err) {
-        res.status(500).send("Internal error");
-      } else {
-        const newHonor = honor;
-        newHonor.title = req.body.title;
-        newHonor.englishTitle = req.body.englishTitle;
-        newHonor.year = req.body.year;
-        newHonor.applyBeginAt = req.body.applyBeginAt;
-        newHonor.applyEndAt = req.body.applyEndAt;
-        newHonor.updatedAt = new Date().toISOString();
-
-        newHonor.save(error => {
-          if (error) {
-            res.status(500).send("Internal error");
-          } else {
-            res.setHeader("Content-Type", "charset=utf-8");
-            res.end("Successfully updated");
-          }
-        });
-      }
-    });
-  }
-});
-
-/**
- * Delete an existing honor.
- * @param {string} token
- * @param {number} id
- * @returns {string}
+ * DELETE
+ * 删除特定荣誉
+ * @param {String} id 删除荣誉的 ID
+ * @returns No Content 或 Not Found
  */
 router.delete(
   "/:id",
   verifyToken,
-  authorizationVerifier("write"),
+  verifyAuthorizations(["write"]),
   (req, res) => {
-    const honorExists = modelExistsVerifier(Honor, req.params.id);
-    if (honorExists === null) {
-      res.status(500).send("Internal error");
-    } else if (honorExists === false) {
-      res.status(404).send("Model not found");
-    } else {
-      Honor.remove({ id: req.params.id }, err => {
-        if (err) {
-          res.status(500).send("Internal error");
-        } else {
-          res.setHeader("Content-Type", "charset=utf-8");
-          res.end("Successfully deleted");
-        }
-      });
-    }
+    Honor.findByIdAndDelete(req.params.id, (err, honor) => {
+      if (err) {
+        res.status(500).send("500 Internal server error.");
+      } else if (!honor) {
+        res.status(404).send("404 Not Found: Honor does not exist.");
+      } else {
+        res.status(204).send("204 No Content.");
+      }
+    });
   }
 );
 
