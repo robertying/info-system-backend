f862f74cc1bdde853e5f4e22144502836f300cfc
Robert Ying <yingrui205@gmail.com>
2018-10-06 15:32:14 -0600

    Add thank letter archive download

--- routes/thankLetters.js
+++ routes/thankLetters.js
@@ -1,128 +1,249 @@
 const express = require("express");
 const path = require("path");
 const Application = require("../models/application");
+const Student = require("../models/student");
 const existenceVerifier = require("../helpers/existenceVerifier");
 const verifyToken = require("../middlewares/verifyToken");
 const verifyAuthorizations = require("../middlewares/verifyAuthorizations");
 const pdfMake = require("pdfmake");
+const fs = require("fs");
+const JSZip = require("jszip");
+const rimraf = require("rimraf");
 
 const router = express.Router();
 
-function createPdfBinary(docDefinition, callback) {
-  const fontDescriptors = {
-    SimHei: {
-      normal: path.join(__dirname, "..", "/fonts/simhei.ttf"),
-      bold: path.join(__dirname, "..", "/fonts/simhei.ttf"),
-      italics: path.join(__dirname, "..", "/fonts/simhei.ttf"),
-      bolditalics: path.join(__dirname, "..", "/fonts/simhei.ttf")
-    },
-    SimSun: {
-      normal: [path.join(__dirname, "..", "/fonts/simsun.ttc"), "SimSun"],
-      bold: [path.join(__dirname, "..", "/fonts/simsun.ttc"), "SimSun"],
-      italics: [path.join(__dirname, "..", "/fonts/simsun.ttc"), "SimSun"],
-      bolditalics: [path.join(__dirname, "..", "/fonts/simsun.ttc"), "SimSun"]
-    }
-  };
+const fontDescriptors = {
+  SimHei: {
+    normal: path.join(__dirname, "..", "/fonts/simhei.ttf"),
+    bold: path.join(__dirname, "..", "/fonts/simhei.ttf"),
+    italics: path.join(__dirname, "..", "/fonts/simhei.ttf"),
+    bolditalics: path.join(__dirname, "..", "/fonts/simhei.ttf")
+  },
+  SimSun: {
+    normal: [path.join(__dirname, "..", "/fonts/simsun.ttc"), "SimSun"],
+    bold: [path.join(__dirname, "..", "/fonts/simsun.ttc"), "SimSun"],
+    italics: [path.join(__dirname, "..", "/fonts/simsun.ttc"), "SimSun"],
+    bolditalics: [path.join(__dirname, "..", "/fonts/simsun.ttc"), "SimSun"]
+  }
+};
 
-  const printer = new pdfMake(fontDescriptors);
-  const doc = printer.createPdfKitDocument(docDefinition);
+const createPdfBinary = docDefinition => {
+  return new Promise(resolve => {
+    const printer = new pdfMake(fontDescriptors);
+    const doc = printer.createPdfKitDocument(docDefinition);
 
-  let chunks = [];
+    let chunks = [];
 
-  doc.on("data", chunk => {
-    chunks.push(chunk);
+    doc.on("data", chunk => {
+      chunks.push(chunk);
+    });
+
+    doc.on("end", () => {
+      resolve(Buffer.concat(chunks));
+    });
+
+    doc.end();
   });
-
-  doc.on("end", () => {
-    callback(Buffer.concat(chunks));
-  });
-
-  doc.end();
-}
+};
 
 router.get(
   "/",
   verifyToken,
   verifyAuthorizations(["read"]),
   async (req, res) => {
-    const title = req.query.title;
-    const id = req.query.id;
-    if (!title || !id) {
-      return res.status(422).send("422 Unprocessable Entity: Missing queries.");
-    }
-
-    const application = await existenceVerifier(Application, {
-      _id: req.query.id
-    });
-    if (
-      application &&
-      application.scholarship &&
-      application.scholarship.contents &&
-      application.scholarship.contents[title]
-    ) {
-      const content = application.scholarship.contents[title].content;
-      const paras = content.split("\n");
-      let parasDefinitions = [
-        {
-          text: "感谢信",
-          style: "heading"
-        },
-        {
-          text: application.scholarship.contents[title].salutation + "：",
-          style: "salutation"
-        }
-      ];
-      for (let index = 0; index < paras.length; index++) {
-        const text = paras[index];
-        parasDefinitions.push({
-          text: text,
-          style: "normal"
-        });
-      }
-      const docDefinition = {
-        content: parasDefinitions,
-        styles: {
-          heading: {
-            fontSize: 15,
-            font: "SimHei",
-            bold: true,
-            alignment: "center",
-            lineHeight: 1.5,
-            marginTop: 40
-          },
-          salutation: {
-            fontSize: 12,
-            font: "SimSun",
-            lineHeight: 2,
-            marginLeft: 50,
-            marginRight: 50,
-            marginTop: 15,
-            marginBottom: 5,
-            preserveLeadingSpaces: false
-          },
-          normal: {
-            fontSize: 12,
-            font: "SimSun",
-            lineHeight: 2,
-            marginLeft: 50,
-            marginRight: 50,
-            preserveLeadingSpaces: false,
-            leadingIndent: 25
+    if (req.query.grade) {
+      Application.find({}, async (err, applications) => {
+        if (err) {
+          res.status(500).send("500 Internal server error.");
+        } else {
+          for (let index = 0; index < applications.length; index++) {
+            const application = applications[index];
+            const student = await existenceVerifier(Student, {
+              id: application.applicantId
+            });
+            if (student) {
+              applications[index].class = student.class;
+            }
           }
+          applications = applications.filter(
+            n => n.class[1] === req.query.grade
+          );
+
+          let dir;
+          if (process.NODE_ENV === "production") {
+            dir = `/tmp/感谢信-无${req.query.grade}年级`;
+          } else {
+            dir = path.join(
+              __dirname,
+              "..",
+              `/data/感谢信-无${req.query.grade}年级`
+            );
+          }
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+          }
+          for (let index = 0; index < applications.length; index++) {
+            const application = applications[index];
+            if (
+              application &&
+              application.scholarship &&
+              application.scholarship.contents
+            ) {
+              const titles = Object.keys(application.scholarship.contents);
+              for (let index = 0; index < titles.length; index++) {
+                const title = titles[index];
+                const content = application.scholarship.contents[title].content;
+                const paras = content.split("\n");
+                let parasDefinitions = [
+                  {
+                    text: "感谢信",
+                    style: "heading"
+                  },
+                  {
+                    text:
+                      application.scholarship.contents[title].salutation + "：",
+                    style: "salutation"
+                  }
+                ];
+                for (let index = 0; index < paras.length; index++) {
+                  const text = paras[index];
+                  parasDefinitions.push({
+                    text: text,
+                    style: "normal"
+                  });
+                }
+                const docDefinition = {
+                  content: parasDefinitions,
+                  styles: {
+                    heading: {
+                      fontSize: 15,
+                      font: "SimHei",
+                      bold: true,
+                      alignment: "center",
+                      lineHeight: 1.5,
+                      marginTop: 40
+                    },
+                    salutation: {
+                      fontSize: 12,
+                      font: "SimSun",
+                      lineHeight: 2,
+                      marginLeft: 50,
+                      marginRight: 50,
+                      marginTop: 15,
+                      marginBottom: 5,
+                      preserveLeadingSpaces: false
+                    },
+                    normal: {
+                      fontSize: 12,
+                      font: "SimSun",
+                      lineHeight: 2,
+                      marginLeft: 50,
+                      marginRight: 50,
+                      preserveLeadingSpaces: false,
+                      leadingIndent: 25
+                    }
+                  }
+                };
+
+                const binary = await createPdfBinary(docDefinition);
+                fs.writeFileSync(
+                  `${dir}/${application.applicantName}-${
+                    application.applicantId
+                  }-${title}.pdf`,
+                  binary
+                );
+              }
+            }
+          }
+          const zip = new JSZip();
+          const folder = zip.folder(dir);
+          fs.readdirSync(dir).forEach(file => {
+            folder.file(file, fs.readFileSync(path.join(dir, file)));
+          });
+          folder.generateAsync({ type: "nodebuffer" }).then(blob => {
+            res.set("Content-Type", "application/zip");
+            res.status(200).send(blob);
+            rimraf(dir, () => {});
+          });
         }
-      };
-      createPdfBinary(
-        docDefinition,
-        binary => {
+      });
+    } else {
+      const title = req.query.title;
+      const id = req.query.id;
+      if (!title || !id) {
+        return res
+          .status(422)
+          .send("422 Unprocessable Entity: Missing queries.");
+      }
+
+      const application = await existenceVerifier(Application, {
+        _id: req.query.id
+      });
+      if (
+        application &&
+        application.scholarship &&
+        application.scholarship.contents &&
+        application.scholarship.contents[title]
+      ) {
+        const content = application.scholarship.contents[title].content;
+        const paras = content.split("\n");
+        let parasDefinitions = [
+          {
+            text: "感谢信",
+            style: "heading"
+          },
+          {
+            text: application.scholarship.contents[title].salutation + "：",
+            style: "salutation"
+          }
+        ];
+        for (let index = 0; index < paras.length; index++) {
+          const text = paras[index];
+          parasDefinitions.push({
+            text: text,
+            style: "normal"
+          });
+        }
+        const docDefinition = {
+          content: parasDefinitions,
+          styles: {
+            heading: {
+              fontSize: 15,
+              font: "SimHei",
+              bold: true,
+              alignment: "center",
+              lineHeight: 1.5,
+              marginTop: 40
+            },
+            salutation: {
+              fontSize: 12,
+              font: "SimSun",
+              lineHeight: 2,
+              marginLeft: 50,
+              marginRight: 50,
+              marginTop: 15,
+              marginBottom: 5,
+              preserveLeadingSpaces: false
+            },
+            normal: {
+              fontSize: 12,
+              font: "SimSun",
+              lineHeight: 2,
+              marginLeft: 50,
+              marginRight: 50,
+              preserveLeadingSpaces: false,
+              leadingIndent: 25
+            }
+          }
+        };
+
+        createPdfBinary(docDefinition).then(binary => {
           res.set("Content-Type", "application/pdf");
           res.status(200).send(binary);
-        },
-        error => {
-          res.status(500).send("500 Internal server error.");
-        }
-      );
-    } else {
-      res.status(404).send("404 Not Found: Application does not exist.");
+        });
+      } else {
+        res.status(404).send("404 Not Found: Application does not exist.");
+      }
     }
   }
 );
